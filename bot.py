6593ec5174da6241bab0b8a7f13f0826d2ad6b9c
nevy11 <smongare2003@gmail.com>
2024-10-18 16:33:45 +0300

    update bot again

--- bot.py
+++ bot.py
@@ -3,25 +3,36 @@ from convert import QA_input
 from qa_input import QA_input as QAI
 import random
 
+"""
+the model_name roberta-base-squad2 model is what we are using
+We'll use pipeline which is a much simpler way to generate output
+loop over the queries
+"""
 model_name = 'deepset/roberta-base-squad2'
 qa = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
 while True:
     answers_found = []
     number_of_found_answers = 0
     question_asked = input("Question: ")
     found = False
     if question_asked == "quit" or question_asked == "\q":
-        
         break
     if question_asked == "help":
         help_words = "help => display's the help contents\nquit or \q => breaks the loops and stops"
         print(help_words)
         found=True
     count = 0
+    """
+    loop over the QA_input that is from the changed output file.json
+    in one loop, compare whether the context matches the question asked, if it matches, generates the answer using the qa model stored by the pipeline.
+    Take in the question asked
+    """
     for index in QA_input:
+        # print(index)
         if index['Context'] == question_asked:
             answer = qa(question_asked, QA_input[count]['Response'])
-            # print(answer['answer'])
+            print(answer['answer'])
             found=True
             number_of_found_answers += 1
             answers_found.append(answer['answer'])
@@ -39,31 +50,34 @@ while True:
             count +=1
 
     if not found:
+         count = 0
          for index in QAI:
-            if index['Context'] == question_asked:
-                answer = qa(question_asked, QAI[count]['Response'])
-                # print(answer['answer'])
+            print(index)
+            if index['Context'] == question_asked.lower():
+                answer = qa(question_asked.lower(), QAI[count]['Response'])
+                print(answer['answer'])
                 found=True
                 number_of_found_answers += 1
                 answers_found.append(answer['answer'])
             count +=1
-            if not found:
-                count = 0
-                for index in QAI:
-                    if index['Context'] == question_asked + "?":
-                        answer = qa(question_asked, QAI[count]['Response'])
-                        # print(answer['answer'])
-                        found=True
-                        number_of_found_answers += 1
-                        answers_found.append(answer['answer'])
+    if not found:
+        count = 0
+        for index in QAI:
+            if index['Context'] == question_asked + "?":
+                answer = qa(question_asked + "?", QAI[count]['Response'])
+                print(answer['answer'])
+                found=True
+                number_of_found_answers += 1
+                answers_found.append(answer['answer'])
 
-                    count +=1
+            count +=1
             
     if not found:
             answer_trial = qa(QA_input[0]['Context'], QA_input[0]['Response'])
             print(answer_trial['answer'])
     if found:
-            index = random.randrange(0, number_of_found_answers+1)
+            print("number_of_found_answers", number_of_found_answers)
+            index = random.randrange(0, number_of_found_answers)
             print("answers_found: ", answers_found)
             print("Selecting: ", answers_found[index])
     
--- convert.py
+++ convert.py
@@ -1,16 +1,6 @@
 import json
 
-# Step 1: Open the JSON file and process each line separately
-with open('training_data/combined_dataset.json', 'r') as file:
-    data = []
-    for line in file:
-        data.append(json.loads(line))  # Load each JSON object line by line
+with open('output_file.json', 'r') as file:
+    data = json.load(file)
 
-# Step 2: Now 'data' contains a list of all your JSON objects
-# print(data)
 QA_input = data
-with open('/media/nevy11/myHardDisk/work/MentalDiaryApp/mentalHealthdiaryAI/output_file.json', 'w') as file:
-    # Step 2: Save the Python dictionary/list back to the file as JSON
-    json.dump(data, file, indent=4)  # 'indent=4' makes the JSON file easier to read
-
-print("Data saved successfully!")
